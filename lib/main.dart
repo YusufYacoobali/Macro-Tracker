20bcad27d333c604d03e56d062c1585715039160
Yusuf Yacoobali <yyacoobali@gmail.com>
2022-05-29 19:31:33 +0100

    add better query

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MacroApp extends StatelessWidget {
   }
 }
 
-//check if text string has space, put %20 in between words
+// new page on clicking food with all info
 //if no results, show nothing showed up/add new food button (does nothing for now)
 // make food object so it can easily be shown
 //start on main page, statistics
--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -25,26 +25,28 @@ class _SearchScreenState extends State<SearchScreen> {
             TextField(
               //controller: textController,
               onSubmitted: (text) {
+                foodList.clear();
                 foodData(text);
-                // setState(() {
-                //   foodList = foodData();
-                // });
+                foodList.clear();
               },
             ),
-            Expanded(
-              child: ListView.builder(
-                  itemCount: foodList.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      title: Text(foodList[index]["lowercaseDescription"]),
-                      //kcal (not sure on portion size)
-                      subtitle: Text(foodList[index]["foodNutrients"][3]
-                              ["value"]
-                          .toString()),
-                    );
-                    ;
-                  }),
-            )
+            foodList.isEmpty
+                ? const Text("No results found")
+                : Expanded(
+                    child: ListView.builder(
+                        itemCount: foodList.length,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            title:
+                                Text(foodList[index]["lowercaseDescription"]),
+                            //kcal (not sure on portion size)=3, 0 = protein
+                            subtitle: Text(foodList[index]["foodNutrients"][0]
+                                    ["value"]
+                                .toString()),
+                          );
+                          ;
+                        }),
+                  )
           ],
         ),
       ),
@@ -52,21 +54,29 @@ class _SearchScreenState extends State<SearchScreen> {
   }
 
   foodData(text) async {
-    //check if text string has space, put %20 in between words
+    String query = text;
+
+    if (text.indexOf(' ') > 0) {
+      query = text.replaceAll(' ', '%20');
+    }
+
+    print(query);
 
     //using own api key
     final response = await http.get(Uri.parse(
-        'https://api.nal.usda.gov/fdc/v1/foods/search?api_key=tfBwCq9vhXCUm6Gsdb9haTW6CjAhqXri8Lg0P2Um&query=${text}'));
+        'https://api.nal.usda.gov/fdc/v1/foods/search?api_key=tfBwCq9vhXCUm6Gsdb9haTW6CjAhqXri8Lg0P2Um&query=$query'));
 
     if (response.statusCode == 200) {
       // If server returns an OK response, parse the JSON.
       var jsonResponse = json.decode(response.body) as Map<String, dynamic>;
-      //print(jsonResponse['foods'].length); //length of 1 page = 50
-      foodList.clear();
+      print(jsonResponse['foods'].length); //length of 1 page = 50
+      //foodList.clear();
 
       setState(() {
         foodList.addAll(jsonResponse['foods']);
       });
+
+      print(foodList);
       //each food has a food num and food nutritents, food nutrients has nutrientid/name + amount for each nutrient
     } else {
       // If that response was not OK, throw an error.
